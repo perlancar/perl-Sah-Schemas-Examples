2b653a59d873ea3596e1a4d9ab688580561418c0
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-05-27 00:14:30 +0700

    Add some examples (tests)

--- lib/Sah/Schema/example/array_of_int.pm
+++ lib/Sah/Schema/example/array_of_int.pm
@@ -1,11 +1,19 @@
 package Sah::Schema::example::array_of_int;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 our $schema = [array => {
     summary => 'Array of integers',
     of => ["int", {req=>1}, {}],
+    examples => [
+        {value=>[], valid=>1},
+        {value=>[1,0,-1], valid=>1},
+        {value=>1, valid=>0},
+        {value=>[1, 1.1], valid=>0},
+    ],
 }, {}];
 
 1;
