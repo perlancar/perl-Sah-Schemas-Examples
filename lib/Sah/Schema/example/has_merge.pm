32450a609ba9e550823a6b65f1fbdf73e14894a1
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-27 21:25:20 +0700

    [doc] Add description to example::has_merge schema

--- lib/Sah/Schema/example/has_merge.pm
+++ lib/Sah/Schema/example/has_merge.pm
@@ -1,10 +1,21 @@
 package Sah::Schema::example::has_merge;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 our $schema = [posint => {
     summary => 'Even integer',
+    description => <<'_',
+
+This schema is based on "posint", which is ["int", {min=>1}], and adds another
+clause div_by=>2. However, this schema also deletes the min=>1 clause using
+merge key: merge.delete.min=>undef. Thus, the resolved result becomes ["int",
+{div_by=>2}] which is basically "even integer". Without the merge key, this
+schema would become "positive even integer."
+
+_
     'merge.delete.min' => undef,
     div_by => 2,
 }, {}];
